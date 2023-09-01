f31661bb5468b60b47c645731894faa215b2b0f7
healthyfun <125756752+codedeerde@users.noreply.github.com>
2023-09-01 12:57:12 +0200

    YouTube

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="https://www.buymeacoffee.com/codedeer">Kauf mir einen Kaffee</a>
+            <a class="nav-link active" aria-current="page" href="https://youtube.com/@codedeer?si=GvmcAfN8zBwtMYH9">YouTube</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Quiz</a>
